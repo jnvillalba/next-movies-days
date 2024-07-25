e6af13262f2844e8b7c8975b397b352b37f201c6
Joaquin Villalba <joaquinvillalba8@gmail.com>
2024-07-25 15:34:17 -0300

    noir

--- src/Lists/Sony/SonyList.js
+++ src/Lists/Sony/SonyList.js
@@ -49,6 +49,16 @@ const SonyList = [
     estreno: "TBA",
   },
   {
+    id: 7,
+    titulo: "Spider-man Noir",
+    tipo: "Sony",
+    poster:
+      "https://assets.mycast.io/posters/spider-man-noir-shot-in-b-w-fan-casting-poster-387387-medium.jpg?1696486279",
+    poster2:
+      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1ee6f212-c270-4752-9e11-17dc812133f5/df190s9-bd753ca7-a0c1-48c7-9542-44d655f9d780.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzFlZTZmMjEyLWMyNzAtNDc1Mi05ZTExLTE3ZGM4MTIxMzNmNVwvZGYxOTBzOS1iZDc1M2NhNy1hMGMxLTQ4YzctOTU0Mi00NGQ2NTVmOWQ3ODAucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.bluV6utrwD7zDzjSGaaX2r2yvjLwYnNzbFguRjvmfrs",
+    estreno: "TBA",
+  },
+  {
     id: 5,
     titulo: "Silk: Spider-Society",
     tipo: "Sony",
