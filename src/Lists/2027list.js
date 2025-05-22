b2520553d78b71621ec1f3439c3a01edbd59068e
Joaquin Villalba <68406893+jnvillalba@users.noreply.github.com>
2025-05-22 20:24:41 -0300

    Update 2027list.js

--- src/Lists/2027list.js
+++ src/Lists/2027list.js
@@ -6,7 +6,7 @@ const content2027List = [
     tipo: "Pelicula",
     poster2:
       "https://assets.mycast.io/posters/avengers-secret-wars-fan-casting-poster-400102-medium.jpg?1700007454",
-    estreno: "07/05/2027",
+    estreno: "17/12/2027",
     poster:
       "https://a.ltrbxd.com/resized/film-poster/9/0/3/8/9/0/903890-avengers-secret-wars-0-1000-0-1500-crop.jpg?v=6e091b3820",
   },
