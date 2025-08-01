4ab8ddb5f8940c2c3e4ea0fc4e75c502b9e81f54
Joaquin Villalba <joaquinvillalba8@gmail.com>
2025-08-01 17:39:58 -0300

    marvel update

--- src/Lists/2025list.js
+++ src/Lists/2025list.js
@@ -23,9 +23,11 @@ const content2025List = [
     id: 7,
     titulo: "Eyes of Wakanda",
     tipo: "Serie",
+    poster2:
+      "https://m.media-amazon.com/images/M/MV5BZjc3YjA1YjYtMzQwMS00MGU4LTgwMGUtYjBkODM4ZjQ2MzM3XkEyXkFqcGc@._V1_FMjpg_UX1086_.jpg",
+    estreno: "01/08/2025",
     poster:
-      "https://i0.wp.com/maxblizz.com/wp-content/uploads/2024/10/Eyes-of-Wakanda.jpg?resize=1024%2C1024&ssl=1",
-    estreno: "06/08/2025",
+      "https://m.media-amazon.com/images/M/MV5BZjhhOTE5ODgtNDFjOS00ZTdlLTgzYjAtNGU1NmM4YzkwYWMxXkEyXkFqcGc@._V1_FMjpg_UX1086_.jpg",
   },
 
   {
--- src/Lists/2026list.js
+++ src/Lists/2026list.js
@@ -7,7 +7,17 @@ const content2026List = [
       "https://assets.mycast.io/posters/mcu-daredevil-born-again-series-season-2-fan-casting-poster-261965-large.jpg?1669298488",
     estreno: "03/03/2026",
   },
-
+  {
+    id: 31062026,
+    titulo: "Spider-Man: Brand New Day",
+    director: "Destin Daniel Cretton",
+    tipo: "Sony",
+    poster2:
+      "https://pbs.twimg.com/media/GxSNYufaAAALN8w?format=jpg&name=large",
+    poster:
+      "https://assets.mycast.io/posters/spider-man-brand-new-day-fan-casting-poster-562737-large.jpg?1743540850",
+    estreno: "31/07/2026",
+  },
   {
     id: 1052026,
     titulo: "Avengers: Doomsday",
@@ -15,20 +25,20 @@ const content2026List = [
     tipo: "Pelicula",
     poster:
       "https://m.media-amazon.com/images/M/MV5BMGNiN2RlZTMtMTkyZC00YjkwLTgyY2QtMDg1ZDNhODQwNWM4XkEyXkFqcGc@._V1_.jpg",
-    estreno: "01/05/2026",
+    estreno: "18/12/2026",
     poster2:
       "https://assets.mycast.io/posters/avengers-doomsday-fan-casting-poster-556932-large.jpg?1741191857",
   },
+
   {
-    id: 31062026,
-    titulo: "Spider-Man: Brand New Day",
-    director: "Destin Daniel Cretton",
-    tipo: "Sony",
+    id: 1052026,
+    titulo: "Marvel Studios: The Punisher",
+    tipo: "Serie",
+    estreno: "TBA - 2026",
     poster2:
-      "https://a.ltrbxd.com/resized/film-poster/8/7/2/8/7/1/872871-spider-man-4-0-1000-0-1500-crop.jpg?v=4aa4f79d52",
+      "https://m.media-amazon.com/images/M/MV5BMzVhNzcyMzItNzkzNi00MWRjLTg1OWEtN2ViMDIyYWNkNTlhXkEyXkFqcGc@._V1_FMjpg_UY2296_.jpg",
     poster:
-      "https://assets.mycast.io/posters/spider-man-brand-new-day-fan-casting-poster-562737-large.jpg?1743540850",
-    estreno: "31/06/2026",
+      "https://m.media-amazon.com/images/M/MV5BNTcyMDA1MjYtODQxYi00YjkyLWIwNTctOGQwNWRmMjM3YmUzXkEyXkFqcGc@._V1_FMjpg_UY3499_.jpg",
   },
 ];
 
