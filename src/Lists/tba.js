186f885ca9e93eeb9145233034df1492b1fabc01
Joaquin Villalba <68406893+jnvillalba@users.noreply.github.com>
2025-10-13 22:41:03 -0300

    Delete 'Vision Quest' from tba list
    Removed 'Vision Quest' entry from the tba array.

--- src/Lists/tba.js
+++ src/Lists/tba.js
@@ -1,14 +1,5 @@
 const tba = [
-  {
-    id: 14,
-    titulo: "Vision Quest",
-    tipo: "Serie",
-    poster:
-      "https://assets.mycast.io/posters/vision-quest-fan-casting-poster-273537-medium.jpg?1672259229",
-    estreno: "TBA",
-    poster2:
-      "https://assets.mycast.io/posters/vision-quest-fan-casting-poster-475062-large.jpg?1717639316",
-  },
+
 
   {
     id: 12,
