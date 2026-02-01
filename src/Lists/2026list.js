c3e6fc7a88a2778f7f8fcbbf70bd279ca7741885
Joaquin Villalba <joaquinvillalba8@gmail.com>
2026-02-01 17:49:25 -0300

    Update 2026 and SW lists with new entries and corrected details

--- src/Lists/2026list.js
+++ src/Lists/2026list.js
@@ -1,31 +1,21 @@
 const content2026List = [
   {
-    id: 121225,
-    titulo: "Wonder Man",
-    tipo: "Serie",
-    poster2:
-      "https://i0.wp.com/maxblizz.com/wp-content/uploads/2024/10/Wonder-Man.jpg?resize=1024%2C1024&ssl=1",
-    estreno: "27/01/2026",
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BOTRmMDVjYTMtZDQxYS00YjhlLTkyZTUtYjFhMDRlM2QyOThkXkEyXkFqcGc@._V1_FMjpg_UX1086_.jpg",
-  },
-  {
     id: 3032026,
     titulo: "Daredevil Born Again S2",
     tipo: "Serie",
     poster:
+      "https://m.media-amazon.com/images/M/MV5BNDBkMWRhMzEtM2M0Ny00OGZhLThkZGMtMTY1NWUwZWNhODdiXkEyXkFqcGc@._V1_FMjpg_UX1086_.jpg",
+    poster2:
       "https://assets.mycast.io/posters/mcu-daredevil-born-again-series-season-2-fan-casting-poster-261965-large.jpg?1669298488",
-    estreno: "03/03/2026",
+    estreno: "24/03/2026",
   },
   {
     id: 31062026,
     titulo: "Spider-Man: Brand New Day",
     director: "Destin Daniel Cretton",
     tipo: "Sony",
-    // poster2:
-    //   "https://pbs.twimg.com/media/GxSNYufaAAALN8w?format=jpg&name=large",
     poster2:
-      "https://scontent.cdninstagram.com/v/t51.82787-15/528726856_18066481547183453_5865312703609289687_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=100&ig_cache_key=MzY5NDcwMjQyMTU0NTcwMTU5NA%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjE0NDB4MTgwMC5zZHIuQzMifQ%3D%3D&_nc_ohc=rgZvmTqV2uIQ7kNvwGUa__Y&_nc_oc=AdkMKb9dov7eg0LqdyaQGXZlCfc8qWcnEoWodeLO0PfSzBHYhlzVFGCeEdEL6G9XXIc&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.cdninstagram.com&_nc_gid=ANt6-vfDgKDwbktTbXGOwQ&oh=00_Afj-hNRm5J4DQoR7u7i0qrmMhW97rMpYMT1Q9UXEy_76sg&oe=6927CC4E",
+      "https://scontent.cdninstagram.com/v/t51.82787-15/530372383_18500074702064564_8499172931460298941_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=105&ig_cache_key=MzY5NjQ2MTYwODUxMDQyMzU0OA%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjE0NDB4MTkxOC5zZHIuQzMifQ%3D%3D&_nc_ohc=utsledDksUIQ7kNvwHTX51A&_nc_oc=AdnSXmtu5viAoFQji2gGdgYbWes8LjVqsPCckGyDsHgIMHXOmmPUhInswNGsmeoY83w&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.cdninstagram.com&_nc_gid=kxLmyxe1g_ZMoUHWtIgnaQ&oh=00_Afuyz53SXHGf_kRGMo6LjiNE7OR0jBnawsfr5QOqjy5rYA&oe=69858E18",
     poster:
       "https://assets.mycast.io/posters/spider-man-brand-new-day-fan-casting-poster-562737-large.jpg?1743540850",
     estreno: "31/07/2026",
--- src/Lists/SW/SWList.js
+++ src/Lists/SW/SWList.js
@@ -1,11 +1,34 @@
 const SWList = [
   {
-    id: 4,
-    titulo: "Star Wars: Visions - S3",
+    id: 726,
+    titulo: "Star Wars: Maul - Shadow Lord",
     tipo: "Serie",
     poster:
-      "https://m.media-amazon.com/images/M/MV5BMWRmZmMzMWYtODMyZS00Y2VkLTk3ZGQtMTY0MTQwMTRlZGU2XkEyXkFqcGc@._V1_FMjpg_UX729_.jpg",
-    estreno: "29/10/2025",
+      "https://m.media-amazon.com/images/M/MV5BODdkZDNlZjAtZGM1MS00YTE1LWJkMzItZWNhYTIzMjY1YzE4XkEyXkFqcGc@._V1_FMjpg_UX1086_.jpg",
+    poster2: "",
+    estreno: "06/04/2026",
+  },
+  {
+    id: 8,
+    titulo: "The Mandalorian & Grogu",
+    director: "Jon Favreau",
+    tipo: "PelaSW",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BNjYwYzM4OTUtMDhmMy00ZTZiLTk0YzMtZGQ1MTRhMDNjZjMzXkEyXkFqcGc@._V1_FMjpg_UX1086_.jpg",
+    poster2:
+      "https://m.media-amazon.com/images/M/MV5BNjYwYzM4OTUtMDhmMy00ZTZiLTk0YzMtZGQ1MTRhMDNjZjMzXkEyXkFqcGc@._V1_FMjpg_UX1086_.jpg",
+    estreno: "22/05/2026",
+  },
+  {
+    id: 280527,
+    titulo: "Star Wars: Starfighter",
+    director: "Shawn Levy",
+    tipo: "PelaSW",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BZWFkM2JmMDktNWEyNS00NGRkLTk3NDMtMzA5YjQyMTM0NWI5XkEyXkFqcGc@._V1_FMjpg_UX1200_.jpg",
+    poster2:
+      "https://m.media-amazon.com/images/M/MV5BODQyYjQ5NjYtNzY2NC00OGY3LWJhY2YtNTBhNmE5NWEyMGUyXkEyXkFqcGc@._V1_FMjpg_UX1200_.jpg",
+    estreno: "28/05/2027",
   },
   {
     id: 5,
@@ -35,26 +58,7 @@ const SWList = [
     poster2: "",
     estreno: "TBA",
   },
-  {
-    id: 726,
-    titulo: "Star Wars: Shadow Lord",
-    tipo: "Serie",
-    poster:
-      "https://www.tvtime.com/_next/image?url=https%3A%2F%2Fartworks.thetvdb.com%2Fbanners%2Fv4%2Fseries%2F462715%2Fposters%2F680206e0205b9.jpg&w=640&q=75",
-    poster2: "",
-    estreno: "TBA - 2026",
-  },
 
-  {
-    id: 8,
-    titulo: "The Mandalorian & Grogu",
-    director: "Jon Favreau",
-    tipo: "PelaSW",
-    poster2: "https://m.media-amazon.com/images/I/71YDdgOqzkL._AC_SY879_.jpg",
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BNjYwYzM4OTUtMDhmMy00ZTZiLTk0YzMtZGQ1MTRhMDNjZjMzXkEyXkFqcGc@._V1_FMjpg_UX1086_.jpg",
-    estreno: "22/05/2026",
-  },
   {
     id: 10,
     titulo: "New Jedi Order",
@@ -71,22 +75,8 @@ const SWList = [
     director: "James Mangold",
     tipo: "PelaSW",
     poster:
-      "https://assets.mycast.io/posters/wonder-woman-fan-casting-poster-101983-large.jpg?1739390862",
-    poster2:
       "https://assets.mycast.io/posters/dawn-of-the-jedi-fan-casting-poster-318427-medium.jpg?1683430708",
     estreno: "17/12/2027",
-  },
-
-  {
-    id: 280527,
-    titulo: "Star Wars: Starfighter",
-    director: "Shawn Levy",
-    tipo: "PelaSW",
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BZWFkM2JmMDktNWEyNS00NGRkLTk3NDMtMzA5YjQyMTM0NWI5XkEyXkFqcGc@._V1_FMjpg_UX1200_.jpg",
-    poster2:
-      "https://m.media-amazon.com/images/M/MV5BODQyYjQ5NjYtNzY2NC00OGY3LWJhY2YtNTBhNmE5NWEyMGUyXkEyXkFqcGc@._V1_FMjpg_UX1200_.jpg",
-    estreno: "28/05/2027",
   },
 
   {
