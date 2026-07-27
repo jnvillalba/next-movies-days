08fa4f328257df9fa7b611a02d2b660247b5e1ee
Joaquin Villalba <joaquinvillalba8@gmail.com>
2026-07-27 11:25:13 -0300

    feat: implement app routing, dynamic navbar, and 2028 movie list data

---
+++ src/Lists/2028list.js
@@ -0,0 +1,24 @@
+const content2028List = [
+  {
+    id: 72028,
+    titulo: "Ghost Rider",
+    director: "Shawn Levy",
+    tipo: "Pelicula",
+    poster2: "",
+    estreno: "30/07/2028",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BZDFhYmQxOTQtMGQ4My00OWM2LTk1MjYtMjgxMmNlYjc1NmEwXkEyXkFqcGc@._V1_FMjpg_UX1080_.jpg",
+  },
+  {
+    id: 151228,
+    titulo: "Black Panther III",
+    director: "Ryan Coogler",
+    tipo: "Pelicula",
+    poster2: "",
+    estreno: "15/12/2028",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BNjNhNzIzNWQtMGNiYy00YTM5LWE2YzQtNjBkNTRjNDU4ZjM3XkEyXkFqcGc@._V1_FMjpg_UX1080_.jpg",
+  },
+];
+
+export { content2028List };
